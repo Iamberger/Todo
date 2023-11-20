dbfb0cbb82edeaae79fcc47bbc1533ccb4c03754
mahan shakerian <117858726+Iamberger@users.noreply.github.com>
2023-11-20 18:20:22 +0330

    App.js

--- src/App.js
+++ src/App.js
@@ -2,33 +2,29 @@ import React from 'react';
 import { useState } from 'react';
 import './todo.css';
 
+
 function Todo() {
   const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
-
-  function Change(e) {
-    setInput(e.target.value);
-    console.log(input); 
-  }
+  const [input, setInput] = useState([]);
 
   function Submit(e) {
     setTodos([...todos, input]);
     setInput('');
   }
 
+  function Change(e) {
+    setInput(e.target.value);
+    console.log(input); 
+  }
   return (
     <div>
-      <h1>Todo List</h1>
+      <h1>Todo List </h1>
       <form>
         <input type='text' value={input} onChange={Change} />
-        <button type='button' onClick={Submit}>
-          Add Todo
-        </button>
+        <button type='button' onClick={Submit}> Add Todo </button>
       </form>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
+      <ul>{todos.map((todo, index) =>
+       (<li key={index}>{todo}</li>))}
       </ul>
     </div>
   );
