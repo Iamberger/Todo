a5e86ad5cda3043c9489774f26f0282d7e1ef09a
mahan shakerian <117858726+Iamberger@users.noreply.github.com>
2023-11-20 08:36:28 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+import './todo.css';
+
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  function Change(e) {
+    setInput(e.target.value);
+    console.log(input); 
+  }
+
+  function Submit(e) {
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form>
+        <input type='text' value={input} onChange={Change} />
+        <button type='' onClick={Submit}>
+          Add Tod
+        </button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+}
+
+export default Todo;
